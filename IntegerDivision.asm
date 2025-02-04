d8bac48f554ba05ea1d6ae25e11edb877c27ed15
Zhaokun-Li <hjmh9196@leeds.ac.uk>
2025-02-04 15:38:36 +0000

    1

--- IntegerDivision.asm
+++ IntegerDivision.asm
@@ -1,35 +1,35 @@
     @R4
-    M=0        
+    M=0          // 设 R4 = 0，假设除法有效
 
     @R1
     D=M
     @DIV_ZERO
-    D;JEQ       
+    D;JEQ       // 如果 y == 0，跳转到 DIV_ZERO
 
     @R2
-    M=0         
+    M=0         // 初始化商 m = 0
     @R3
-    M=0         
+    M=0         // 初始化余数 q = 0
 
     @R0
     D=M         
     @X_COPY
-    M=D         
+    M=D         // 复制 x 以防止修改原始值
 
     @R1
     D=M         
     @Y_COPY
-    M=D         
+    M=D         // 复制 y 以防止修改原始值
 
     @X_COPY
     D=M
     @NEG_X
-    D;JLT       
+    D;JLT       // 如果 x < 0，跳转到 NEG_X
 
     @Y_COPY
     D=M
     @NEG_Y
-    D;JLT       
+    D;JLT       // 如果 y < 0，跳转到 NEG_Y
 
     @X_SIGN
     M=1         
@@ -40,7 +40,7 @@
     @X_SIGN
     M=-1        
     @X_COPY
-    M=-M        
+    M=-M        // 取 x 的绝对值
 
 (POS_X)
     @Y_SIGN
@@ -52,7 +52,7 @@
     @Y_SIGN
     M=-1        
     @Y_COPY
-    M=-M        
+    M=-M        // 取 y 的绝对值
 
 (POS_Y)
 (LOOP)
@@ -61,7 +61,7 @@
     @Y_COPY
     D=D-M       
     @DONE
-    D;JLT       
+    D;JLT       // 如果 x < y，跳转到 DONE
 
     @X_COPY
     M=D         
@@ -75,19 +75,19 @@
     @X_COPY
     D=M
     @R3
-    M=D         
+    M=D         // 余数 q = x
 
     @X_SIGN
     D=M
     @Y_SIGN
     D=D*M
     @R2
-    M=D*M        
+    M=D*M       // 调整商的符号
 
     @X_SIGN
     D=M
     @R3
-    M=D*M        
+    M=D*M       // 确保余数符号和 x 一致
 
     @END
     0;JMP       
@@ -106,7 +106,7 @@
 
 (DIV_ZERO)
     @R4
-    M=1         
+    M=1         // 设标志 R4 = 1（无效除法）
     @END
     0;JMP       
 
